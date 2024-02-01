10e323fde2b9fb3b30c3f3b9dcb1fc2907c39a8e
mryeminthu <dhammaarchive@hotmail.com>
2024-02-01 20:13:15 +0630

    Change snow items

--- src/components/Snowfall.jsx
+++ src/components/Snowfall.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Snowfall from "react-snowfall";
 
 const SnowfallComponent = () => {
-  return <Snowfall snowflakeCount={100} />;
+  return <Snowfall snowflakeCount={50} />;
 };
 
 export default SnowfallComponent;
