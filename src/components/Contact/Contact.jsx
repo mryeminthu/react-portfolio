fc8fde7f7aed087eb4173e0acf1cab371c602196
mryeminthu <dhammaarchive@hotmail.com>
2024-02-01 20:12:35 +0630

    Add Contact jsx and css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import SnowfallComponent from "../Snowfall";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h2>Contact Page</h2>
+    <section className="contact-section">
+      <h2>Feel Free to Connect!</h2>
+
+      <div className="contact-content">
+        <div className="contact-info">
+          <p className="contact-text">
+            Please don't hesitate to drop me a message on my Contact Page. I'm
+            excited to hear from you.
+          </p>
+        </div>
+
+        <div className="contact-form">
+          <form>
+            <input type="text" id="name" name="name" />
+
+            <input type="email" id="email" name="email" />
+
+            <textarea id="message" name="message"></textarea>
+
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </div>
+
       <SnowfallComponent />
-    </div>
+    </section>
   );
 };
 
