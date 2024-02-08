73cfd4ea983f0414dd5292cc1af3f26834c50562
mryeminthu <dhammaarchive@hotmail.com>
2024-02-08 18:50:26 +0630

    Add styles to skills popup

--- src/components/About/SkillsPopup.jsx
+++ src/components/About/SkillsPopup.jsx
@@ -8,7 +8,7 @@ const SkillsPopup = ({ onClose }) => {
         <h3>My Skills</h3>
         <div className="skills-content">
           <div className="category">
-            <h4>Frontend</h4>
+            <h3>Frontend</h3>
             <ul>
               <li>
                 <i className="fab fa-html5"></i> HTML
@@ -23,21 +23,18 @@ const SkillsPopup = ({ onClose }) => {
                 <i className="fab fa-react"></i> React
               </li>
               <li>
-                <i className="fab fa-wordpress"></i> WordPress
-              </li>
-              <li>
-                <i className="fab fa-css3"></i> TailwindCSS
+                <i className="fas fa-code"></i> Redux
               </li>
             </ul>
           </div>
           <div className="category">
-            <h4>Backend</h4>
+            <h3>Backend</h3>
             <ul>
               <li>
                 <i className="fas fa-gem"></i> Ruby
               </li>
               <li>
-                <i className="fas fa-gem"></i> RoR
+                <i className="fas fa-gem"></i> Ruby on Rails
               </li>
               <li>
                 <i className="fas fa-database"></i> Postgresql
@@ -45,7 +42,7 @@ const SkillsPopup = ({ onClose }) => {
             </ul>
           </div>
           <div className="category">
-            <h4>Tools</h4>
+            <h3>Frameworks/CMS/Tools</h3>
             <ul>
               <li>
                 <i className="fab fa-git-alt"></i> Git
@@ -55,6 +52,12 @@ const SkillsPopup = ({ onClose }) => {
               </li>
               <li>
                 <i className="fab fa-bootstrap"></i> Bootstrap
+              </li>
+              <li>
+                <i className="fab fa-wordpress"></i> WordPress
+              </li>
+              <li>
+                <i className="fab fa-css3"></i> TailwindCSS
               </li>
             </ul>
           </div>
